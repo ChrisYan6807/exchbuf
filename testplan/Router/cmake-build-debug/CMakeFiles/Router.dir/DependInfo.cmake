cca8fe7228769a34b44fb6ec86fa99c9ede12323
chris <yanchr6807@gmail.com>
2022-09-03 16:58:41 +0100

    presence map

---
+++ testplan/Router/cmake-build-debug/CMakeFiles/Router.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/yanch/Documents/mps/exchbufv2/testplan/Router/main.cpp" "C:/Users/yanch/Documents/mps/exchbufv2/testplan/Router/cmake-build-debug/CMakeFiles/Router.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../cpp"
+  "../../../solutions/LME/source_gen/LME"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ testplan/Router/cmake-build-debug/CMakeFiles/Router.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Router.dir/main.cpp.obj"
+  "Router.exe"
+  "Router.exe.manifest"
+  "Router.lib"
+  "Router.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Router.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
